a896f46216dd2b0ca46f55995e2afd396ad82e15
smmartin1 <smarie333m@gmail.com>
2022-06-23 10:39:13 -0400

    Create NumberOfEvents.js

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value >= 0 && value <= 32) {
+      this.setState({ numberOfEvents: value });
+    } else {
+      console.log('Enter a number between 1 and 32');
+    }
+  };
+
+  render() {
+    return (
+      <div className="CitySearch">
+        <input
+          type="text"
+          className="events"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
+      </div>
+    )
+  }
+}
+
+export default NumberOfEvents;
