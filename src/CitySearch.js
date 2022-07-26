9a0e3cc63f6f9062a888b93816b835579e1dee1c
smmartin1 <smarie333m@gmail.com>
2022-07-26 13:55:28 -0400

    Added placeholder

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -34,6 +34,7 @@ class CitySearch extends Component {
           type="text"
           className="city"
           value={this.state.query}
+          placeholder="City, Country"
           onChange={this.handleInputChanged}
           onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
