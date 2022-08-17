c1b669b6a24885656beb2f9ce966f72b07c15b16
smmartin1 <smarie333m@gmail.com>
2022-08-17 16:51:03 -0400

    Added Welcome Screen

---
+++ src/WelcomeScreen.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './WelcomeScreen.css';
+
+function WelcomeScreen(props) {
+  return props.showWelcomeScreen ?
+    (
+      <div className="WelcomeScreen">
+        <h1>Welcome to the Meet App</h1>
+        <h4>Log in to view upcoming global events for full-stack developers</h4>
+
+        <div className="button_count" align="center">
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                class="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="Google sign-in"
+              />
+            </div>
+
+            <button
+              onClick={() => { props.getAccessToken() }}
+              rel="nofollow noopener"
+              class="btn-text"
+            >
+              <b>Sign in with google</b>
+            </button>
+          </div>
+        </div>
+
+        <a
+          href="https://smmartin1.github.io/meet/privacy.html"
+          rel="nofollow noopener"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    )
+    : null
+}
+
+export default WelcomeScreen;
